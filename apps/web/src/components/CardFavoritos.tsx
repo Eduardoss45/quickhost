aa942e21af058513d98042c7579e58d2f88b369b
Eduardo <temporariac432@gmail.com>
2026-01-07 17:56:04 -0300

    add&fix: migrando frontend react para tyescript e adicionando shadcn com tailwindcss

---
+++ apps/web/src/components/CardFavoritos.tsx
@@ -0,0 +1,51 @@
+import { CiLocationOn } from 'react-icons/ci';
+import useDetalhes from '../hooks/useDetalhes.jsx';
+import useAccommodation from '../hooks/useAccommodation.js';
+import useFavorite from '../hooks/useFavorite.js';
+import { PiTrashSimple } from 'react-icons/pi';
+
+import './css/CardFavoritos.css';
+
+const CardFavoritos = ({ dados }) => {
+  const { accommodationData } = useAccommodation(dados);
+  const creatorData = useDetalhes(accommodationData?.creator) || null;
+  const { userData: name } = creatorData || {};
+  const { isFavorite, toggleFavorite } = useFavorite(dados);
+  const handleFavoriteClick = () => {
+    toggleFavorite();
+    window.location.reload();
+  };
+
+  const imageUrl = `${import.meta.env.VITE_BASE_URL}${accommodationData?.main_cover_image}` || null;
+
+  return (
+    <div className="card-favoritos">
+      <img src={imageUrl} alt={accommodationData?.title || 'Imagem da acomodação'} />
+      <div className="card-favoritos-content">
+        <h2>{accommodationData?.title}</h2>
+        <p>{name?.username || 'Nome do Criador Indisponível'}</p>
+        <p>
+          <strong>R$ {accommodationData?.price}</strong> por noite
+        </p>
+      </div>
+      <div className="card-favoritos-linha"></div>
+      <div className="card-favoritos-location">
+        <span>
+          <CiLocationOn />
+        </span>
+        <span>{accommodationData?.city || 'Cidade Indisponível'}</span>
+      </div>
+      <div className="card-favoritos-linha"></div>
+      <div className="card-favoritos-remover">
+        <button onClick={handleFavoriteClick}>
+          <span>
+            <PiTrashSimple />
+          </span>
+          Remover
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CardFavoritos;
